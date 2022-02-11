5a1fb2b4252e7d37f274af6faf7f0ca5210c6807
JEASSON-ALFONSO <>
2022-02-11 11:20:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,67 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Stack;
+
+class Main {
+  public static void main(String[] args) {
+    new Main();
+  }
+
+  public static LinkedList<String> colaExpresion = new LinkedList<>();
+  public static LinkedList<String> expPos = new LinkedList<>();
+
+  public Main() {
+    Scanner n = new Scanner(System.in);
+    separarExpresion(n.nextLine());
+    Stack<String> pilaOperaciones = new Stack<>();
+    while (!colaExpresion.isEmpty()) {
+      System.out.println(colaExpresion.poll());
+    }
+    // while (!colaExpresion.isEmpty()) {
+    //   String elemento = colaExpresion.poll();
+    //   if (elemento.equals("(")) {
+    //     pilaOperaciones.push(elemento);
+    //   } else if (elemento.equals(")")) {
+    //     while (pilaOperaciones.peek().equals("(")) {
+    //       expPos.offer(pilaOperaciones.pop());
+    //     }
+    //     pilaOperaciones.pop();
+    //   }
+    // }
+  }
+
+  public void separarExpresion(String exp) {
+    String numero = "";
+    String operaciones = "()+-*/^%";
+    String cNumeros = "0123456789.,";
+    boolean termino = true;
+    
+    for (int i = 0; i < exp.length(); i++) {
+      char c = exp.charAt(i);
+      
+      if (operaciones.contains(c + "")) {
+        if (termino == false) {
+          numero += c;
+          colaExpresion.offer(numero);
+          termino = true;
+          numero = "";
+        }
+      } else if (cNumeros.contains(c + "")) {
+        // if (termino && ".,".contains(c + "")){
+        //   numero += "0";
+        // }else{
+          
+        // }
+        numero += c;
+        numero = numero.replace(',', '.');
+        termino = false;
+      }
+      if (termino == false) {
+        colaExpresion.offer(numero);
+        termino = true;
+        numero = "";
+      }
+    }
+  }
+
+}
