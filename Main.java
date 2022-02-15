cb34f5cfee6809367f1ab4ea4e60e4202a28db00
Jeasson Suarez <66092398+JeassonSuarez@users.noreply.github.com>
2022-02-15 01:56:39 +0000

    Infija postfija v1

--- Main.java
+++ Main.java
@@ -7,6 +7,10 @@ class Main {
     new Main();
   }
 
+  int valJerarquiaCola;
+  int valJerarquiaPila;
+  int x=0;
+
   public static LinkedList<String> colaExpresion = new LinkedList<>();
   public static LinkedList<String> expPos = new LinkedList<>();
 
@@ -14,25 +18,75 @@ class Main {
     Scanner n = new Scanner(System.in);
     separarExpresion(n.nextLine());
     Stack<String> pilaOperaciones = new Stack<>();
-    while (!colaExpresion.isEmpty()) {
-      System.out.println(colaExpresion.poll());
-    }
     // while (!colaExpresion.isEmpty()) {
-    //   String elemento = colaExpresion.poll();
-    //   if (elemento.equals("(")) {
-    //     pilaOperaciones.push(elemento);
-    //   } else if (elemento.equals(")")) {
-    //     while (pilaOperaciones.peek().equals("(")) {
-    //       expPos.offer(pilaOperaciones.pop());
-    //     }
-    //     pilaOperaciones.pop();
-    //   }
+    //   System.out.println(colaExpresion.poll());
     // }
+    while (!colaExpresion.isEmpty()) {
+      String elemento = colaExpresion.poll();
+      if(elemento == "%"){
+        valJerarquiaCola = 4;
+      }else if (elemento == "p") {
+        valJerarquiaCola = 3;
+      }else if (elemento == "*" || elemento == "/") {
+        valJerarquiaCola = 2;
+      }else if (elemento == "+" || elemento == "-") {
+        valJerarquiaCola = 1;
+      }
+      
+      if ("%p*+-/".contains(elemento)) {
+        if (pilaOperaciones.empty()){
+          pilaOperaciones.push(elemento);
+        }else{
+          if(pilaOperaciones.peek() == "%"){
+            valJerarquiaPila = 4;
+          }else if (pilaOperaciones.peek() == "p") {
+            valJerarquiaPila = 3;
+          }else if (pilaOperaciones.peek() == "*" || elemento == "/") {
+            valJerarquiaPila = 2;
+          }else if (pilaOperaciones.peek() == "+" || elemento == "-") {
+            valJerarquiaPila = 1;
+          }
+
+          if (valJerarquiaCola == valJerarquiaPila) {
+            System.out.print(pilaOperaciones.pop() + " = " + elemento);
+            pilaOperaciones.push(elemento);
+          }
+
+          if (valJerarquiaCola > valJerarquiaPila) {
+            pilaOperaciones.push(elemento);
+          }
+
+          if (valJerarquiaCola < valJerarquiaPila) {
+            while (!pilaOperaciones.empty()) {
+              System.out.print(pilaOperaciones.pop());
+            }
+            pilaOperaciones.push(elemento);
+          }
+
+        } 
+      }else{
+        if (!elemento.equals(")") && !elemento.equals("(")) {
+          System.out.print(elemento);
+        }
+      }
+      if (elemento.equals(")")) {
+        while (!pilaOperaciones.empty()) {
+          System.out.print(pilaOperaciones.pop());
+        }
+      }
+      //System.out.println(pilaOperaciones.peek());
+      // else if (elemento.equals(")")) {
+      //   while (pilaOperaciones.peek().equals("(")) {
+      //     expPos.offer(pilaOperaciones.pop());
+      //   }
+      //   pilaOperaciones.pop();
+      // }
+    }
   }
 
   public void separarExpresion(String exp) {
     String numero = "";
-    String operaciones = "()+-*/^%";
+    String operaciones = "()+-*/p%";
     String cNumeros = "0123456789.,";
     boolean termino = true;
     
